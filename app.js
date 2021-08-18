c9a032420d231f06c5cec5c09861e5a7da73c1d9
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2021-08-18 19:31:30 -0300

    pusheo toda la app de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const path = require ("path");
+const app = express ();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen (3000, () => console.log ("Servidor esta corriendo"));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/log-in", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/log-in.html"));
+});
