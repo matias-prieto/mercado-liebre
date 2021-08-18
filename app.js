1097a10ba5f9418e8f30e4784966d85a34175a74
mati.prieto98@gmail.com <mati.prieto98@gmail.com>
2021-08-18 20:03:24 -0300

    modifique app.js para que heroku acepte

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express ();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen (3000, () => console.log ("Servidor esta corriendo"));
+app.listen (process.env.PORT || 3000, () => {
+    console.log("Servido esta corriendo");
+})
+// app.listen (3000, () => console.log ("Servidor esta corriendo"));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
